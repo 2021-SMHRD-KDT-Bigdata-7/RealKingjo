8c378ced78e9fbb016090aa8d6307c85b6d2dbcd
kimohhyun7 <kimohhyun7@naver.com>
2022-02-23 22:17:17 +0900

    게시판 모든거

--- logos/MainBoard/admin.py
+++ logos/MainBoard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import life_Question
 # Register your models here.
+
+class life_QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+admin.site.register(life_Question, life_QuestionAdmin)
